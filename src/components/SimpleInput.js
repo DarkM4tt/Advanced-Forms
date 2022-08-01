40e15979d7cd524bd6a10c7680632f9c059044d1
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-08-01 14:37:40 +0530

    Setup basic form both with useState & useRef.

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,43 @@
+import { useState, useRef } from 'react'
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('')
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+
+    if (enteredName.trim() === '') {
+      return
+    }
+
+    console.log(enteredName)
+    const enteredValue = nameInputRef.current.value
+    console.log(enteredValue)
+    setEnteredName('')
+  }
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  )
+}
+
+export default SimpleInput
