9ac21046a8691b56c5f3c4e149b08b22e356eefc
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-08-01 18:06:10 +0530

    Optimized!

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,40 +1,59 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('')
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+  const enteredNameIsValid = enteredName.trim() !== ''
+  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched
+
+  let formIsValid = false
+
+  if (enteredNameIsValid) {
+    formIsValid = true
+  }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value)
   }
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true)
+  }
+
   const formSubmitHandler = (event) => {
     event.preventDefault()
 
-    if (enteredName.trim() === '') {
+    setEnteredNameTouched(true)
+
+    if (!enteredNameIsValid) {
       return
     }
-
-    console.log(enteredName)
-    const enteredValue = nameInputRef.current.value
-    console.log(enteredValue)
     setEnteredName('')
+    setEnteredNameTouched(false)
   }
+
+  const nameInputClasses = nameInputIsInValid
+    ? 'form-control invalid'
+    : 'form-control'
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
       </div>
+      {nameInputIsInValid && (
+        <p className="error-text">Name must not be empty!</p>
+      )}
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   )
